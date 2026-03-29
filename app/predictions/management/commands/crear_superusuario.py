f8a250a803e777dc5d38a591bcf1c4741c13ccc7
yamilpalmiero <yamilpalmiero@gmail.com>
2026-03-29 11:29:45 +0200

    chore: agregar comando crear_superusuario

---
+++ app/predictions/management/commands/crear_superusuario.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@maldinis.app', 'maldinis2026')
+            self.stdout.write('Superusuario creado')
+        else:
+            self.stdout.write('Superusuario ya existe')
